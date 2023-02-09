a7cac0de93f801c9b4152fc96ac3bba1b88df4ef
moreDasha <moredasha@icloud.com>
2023-02-09 21:41:57 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkObject = Object.fromEntries(Object.entries(Object.fromEntries(bracketsConfig)).map(([key,value])=>[value,key]));
+  let checkOpenBrakets = Object.values(checkObject);
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let x = str[i];
+    if (checkOpenBrakets.includes(x)) {
+      if (x === checkObject[x]) {
+        if (stack.length !== 0) {
+          let topE = stack[stack.length - 1];
+          if (x === topE) {
+            stack.pop();
+          } else {
+            stack.push(x);
+          }
+        } else {
+          stack.push(x);
+        }
+      } else {
+        stack.push(x);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      } 
+      let topE = stack[stack.length - 1];
+      if (checkObject[x] === topE) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  } 
+  if (stack.length === 0) {
+    return true
+  } else {
+    return stack.length % 2 === 0
+  }
 }
